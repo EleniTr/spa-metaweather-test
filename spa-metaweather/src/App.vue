a4f68efbe930fbe501ffd8768d31d2636a725f81
Eleni Trachanidou <trachanid@gmail.com>
2022-07-02 02:52:55 +0300

    make search bar

--- spa-metaweather/src/App.vue
+++ spa-metaweather/src/App.vue
@@ -5,41 +5,55 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <form role="search">
+      <input type="text" name="search" id="search" placeholder="Search" v-model="search" />
+      <button type="submit">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+          <path
+            d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z" />
+        </svg>
+      </button>
+    </form>
   </header>
-
   <RouterView />
 </template>
 
 <style>
 @import '@/assets/base.css';
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
 
 header {
   line-height: 1.5;
   max-height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header form{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  height: 10vh;
 }
+
+header svg{
+  width:30px;
+  height: 30px;
+}
+
+#search{
+  width:85%;
+  font-size: 25px;
+  padding-left: 20px;
+}
+
+ header form button {
+  width: 100px;
+  background: azure;
+  }
 
 a,
 .green {
@@ -80,21 +94,20 @@ nav a:first-of-type {
 }
 
 @media (min-width: 1024px) {
+
   body {
     display: flex;
-    place-items: center;
   }
 
   #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
   }
 
   header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    justify-content: center;
   }
 
   header .wrapper {
@@ -115,5 +128,23 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
+}
+
+@media (max-width: 993px) {
+  header form {
+    height: 4vh;
+  }
+    #search {
+      width: 65%;
+      font-size: 15px;
+      padding-left: 10px;
+    }
+    
+    header svg {
+      width: 20px;
+      height: 20px;
+    }
+    
+    
 }
 </style>
--- spa-metaweather/src/components/WelcomeItem.vue
+++ spa-metaweather/src/components/WelcomeItem.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="item">
-    <i>
-      <slot name="icon"></slot>
-    </i>
     <div class="details">
       <h3>
         <slot name="heading"></slot>
@@ -18,10 +15,6 @@
   display: flex;
 }
 
-.details {
-  flex: 1;
-  margin-left: 1rem;
-}
 
 i {
   display: flex;
